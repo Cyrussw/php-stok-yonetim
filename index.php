d6681b874e6774517fa8a0adc05bb0a73be74148
Burak <117117416+Cyrussw@users.noreply.github.com>
2024-01-10 13:33:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,14 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="script.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
     <title>Sairus | Ürün Yönetim</title>
 </head>
 
 <body>
-    <?php include('navbar.php'); ?>
-
     <section class="section">
         <div class="container">
             <h1 class="title is-size-1 has-text-centered">Ürün Ekle</h1>
