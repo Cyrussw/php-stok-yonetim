f516720c70aa39619566fb967186a25735d25f9a
Burak <117117416+Cyrussw@users.noreply.github.com>
2024-01-10 13:33:18 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -4,11 +4,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="script.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
     <title>Sairus | Ürün Yönetim</title>
 </head>
 
 <body>
+    <?php include('navbar.php'); ?>
+
     <section class="section">
         <div class="container">
             <h1 class="title is-size-1 has-text-centered">Ürün Ekle</h1>
@@ -20,11 +23,12 @@
                         <input name="urun_adi" class="input" type="text" placeholder="Ürün Adını Giriniz." required>
                     </div>
                 </div>
-                
+
                 <div class="field">
                     <label class="label">Ürün Stok Adeti</label>
                     <div class="control">
-                        <input name="urun_adet" class="input" type="number" min="1" placeholder="Ürün Stok Adetini Giriniz." required>
+                        <input name="urun_adet" class="input" type="number" min="1"
+                            placeholder="Ürün Stok Adetini Giriniz." required>
                     </div>
                 </div>
 
